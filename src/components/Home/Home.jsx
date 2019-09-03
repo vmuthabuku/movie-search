16be3ee8f10f101bae6bba05c486a0eb3f541e64
vmuthabuku <vmuthabuku@gmail.com>
2019-09-03 04:22:58 +0300

    -- initial commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Layout from '../Layout/Layout';
+// import { Spin } from 'antd';
+
+
+export default class Home extends Component {
+  render() {
+    return(
+      <Layout/>
+    )
+  }
+}
